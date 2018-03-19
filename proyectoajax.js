0a8f22521b8cb520b77c6d07263e52e5acc95542
emeateuve <emeateuve@gmail.com>
2018-03-19 22:55:42 +0100

    funcion de error no funciona, da error XDXD

--- proyectoajax.js
+++ proyectoajax.js
@@ -3,24 +3,46 @@ function buscarPelicula() {
         console.log('antes de ajax')
         $.ajax({
             url: "http://www.omdbapi.com/?s=" + $("#inputPelicula").val() + "&apikey=31b14819",
+            type: "get",
+            dataType: "json",
             success: function (result) {
-                // addPelicula(result);
+                addPelicula(result);
                 console.log(result)
             },
-            error: function () {
-                console.log('HA habido un error compadre')
+            error: function (nopelicula) {
+                noEncontrado(nopelicula);
+                console.log('no existe ompare', nopelicula)
             }
 
         });
-        $("#inputPelicula").val('');
+        // $("#inputPelicula").val('');
     })
 
 };
 
+function noEncontrado(nopelicula) {
+    console.log('no encontrado', nopelicula)
+}
+
+function addPelicula(pelicula) {
+    $('#contenido').empty()
+    $('#contenido').append('<p>Mostrando resultados </p>');
+
+    for (let i = 0; i < pelicula['Search'].length; i++) {
+        $('#contenido').append('<div class="card" style="width: 18rem; display: inline-block;">\n' +
+            '  <img class="card-img-top" src="' + pelicula['Search'][i].Poster + '" alt="Portada no cargada!">\n' +
+            '  <div class="card-body">\n' +
+            '    <h5 class="card-title">' + pelicula['Search'][i].Title + '</h5>\n' +
+            '    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>\n' +
+            '    <a href="#" class="btn btn-primary">Go somewhere</a>\n' +
+            '  </div>\n' +
+            '</div>')
+
+    }
+
+
+}
+
 $(document).ready(function () {
     buscarPelicula();
 });
-//
-// function addPelicula(pelicula) {
-//     for (let i = 0; i < pelicula)
-// }
