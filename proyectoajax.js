dabca0582192626ea59f555de098720d9cd07701
emeateuve <emeateuve@gmail.com>
2018-03-21 07:42:39 +0100

    pilla valor pero incorrectamente

--- proyectoajax.js
+++ proyectoajax.js
@@ -4,7 +4,7 @@ function buscarPelicula() {
         $.ajax({
             url: "http://www.omdbapi.com/?s=" + $("#inputPelicula").val() + "&apikey=31b14819",
             success: function (result) {
-                if (result.Response === 'True'){
+                if (result.Response === 'True') {
                     addPelicula(result);
                 } else {
                     noEncontrado(result, $('#inputPelicula'))
@@ -22,12 +22,12 @@ function buscarPelicula() {
 
 function noEncontrado(nopelicula, nombre) {
     $('#contenido').empty()
-    $('#contenido').append(nopelicula.Error, '<p>No hemos podido encontrar resultados para</p>',nombre.val());
+    $('#contenido').append(nopelicula.Error, '<p>No hemos podido encontrar resultados para</p>', nombre.val());
 
 }
 
 function buscarDetalle(idPelicula) {
-    console.log('ID de la película: ', idPelicula.val())
+    console.log('ID de la película: ', idPelicula)
 }
 
 function addPelicula(pelicula) {
@@ -39,8 +39,8 @@ function addPelicula(pelicula) {
             '  <img class="card-img-top" src="' + pelicula['Search'][i].Poster + '" alt="Portada no cargada!">\n' +
             '  <div class="card-body">\n' +
             '    <h5 class="card-title">' + pelicula['Search'][i].Title + '</h5>\n' +
-            '    <p class="card-text">'+pelicula['Search'][i].Year+'</p>\n' +
-            '    <a href="#" class="btn btn-primary" onclick="'+pelicula['Search'][i].imdbID+'">Ver detalle</a>\n' +
+            '    <p class="card-text">' + pelicula['Search'][i].Year + '</p>\n' +
+            '    <a class="btn btn-primary" onclick="'+buscarDetalle(pelicula['Search'][i].imdbID)+'">Ver detalle</a>\n' +
             '  </div>\n' +
             '</div>')
 
